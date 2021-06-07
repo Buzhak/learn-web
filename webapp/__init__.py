7f007cdc5227271990ec54fdc1fac6e60cf1ec72
Buzhak <Byzhak@ya.ru>
2021-06-07 22:36:53 +0300

    add db sqlalchemy

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, wrappers
 from webapp.weather import weather_by_city
-from webapp.python_org_news import get_python_news
+
+from webapp.model import db, News
 CITY = {'moscow':('55.89','37.47'),'murmansk':('68.97', '33.07')}
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
+    db.init_app(app)
 
     @app.route('/<city>/')
     def index(city):
@@ -13,7 +15,7 @@ def create_app():
         title = 'Новости Python'
         get_city = CITY.get(city, CITY['moscow'])
         weather = weather_by_city(*get_city)
-        get_news_list = get_python_news()
+        get_news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', page_title = title, weather_text = weather, news_list = get_news_list)
 
     return app
--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
 import requests
 from bs4 import BeautifulSoup
+
+from webapp.model import db, News
 
 
 def get_html(url):
@@ -21,21 +25,17 @@ def get_python_news():
             title = news.find('a').text
             url = news.find('a')['href']
             published = news.find('time').text
-            resul_news.append({
-                "title": title,
-                "utl": url,
-                "published": published
-            })
-        return resul_news
-    return False
-      
-    # print(all_news)
+            try:
+                published = datetime.strptime(published, '%Y-%m-%d')
+            except (ValueError):
+                published = datetime.now()
+            save_news(title, url, published)
+     
 
-# if __name__==('__main__'):
-#     html = get_html("https://www.python.org/blogs/")
-#     if html:
-#         news = get_python_news(html)
-#         print(news)
-#         # print('Все ок')
-#         # with open('python.org.html', 'w', encoding = 'utf-8') as f:
-#         #     f.write(html)
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    print(news_exists)
+    if not news_exists:
+        news_news = News(title = title, url = url, published = published)
+        db.session.add(news_news)
+        db.session.commit()
